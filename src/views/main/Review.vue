fa118ce30446ff1fda045899e866be703c0e0e2c
“yuanyu” <yuanyu@email.com>
2022-01-28 15:53:55 +0800

    下班了，溜了溜了

--- src/views/main/Review.vue
+++ src/views/main/Review.vue
@@ -1,21 +1,68 @@
 <template>
-  <div>
-    <el-carousel indicator-position="outside" :autoplay="false" arrow="always">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <el-card class="box-card">
-          <h3>{{item}}</h3>
-        </el-card>
-      </el-carousel-item>
-    </el-carousel>
-    <button @click="preWord()" type="button" class="el-carousel__arrow--left" style=""><i class="el-icon-arrow-left"></i></button>
-    <button @click="nextWord()" type="button" class="el-carousel__arrow--right" style=""><i class="el-icon-arrow-right"></i></button>
+  <div style="">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <div><h5><span>equal[ikwel]</span></h5></div>
+        <div>
+          <i class="el-icon-microphone"></i>
+        </div>
+      </div>
+      <transition name="el-zoom-in-center">
+        <div v-show="show" class="transition-box">
+          <div class="wordContent" @click="hide()">
+            <div>
+              <div >[形]相等的;同等的</div>
+              <div >[动]等于;比得上</div>
+              <div >[名]同等的人;相等物</div>
+            </div>
+            <el-divider style="margin: 15px 0;"></el-divider>
+            <div>
+              <div>列句</div>
+              <div>Nobody can equal him in intelligence</div>
+              <div>Nobody can equal him in intelligence</div>
+              <div>Nobody can equal him in intelligence</div>
+              <div>Nobody can equal him in intelligence</div>
+              <div>Nobody can equal him in intelligence</div>
+              <div>Nobody can equal him in intelligence</div>
+            </div>
+            <el-divider style="margin: 15px 0;"></el-divider>
+            <div>
+              <div>助记</div>
+              <div>Nobody can equal him in intelligence</div>
+              <div>Nobody can equal him in intelligence</div>
+            </div>
+          </div>
+        </div>
+      </transition>
+      <transition name="el-zoom-in-center">
+        <div v-show="!show" class="transition-box">
+          <div class="wordContent"  @click="hide()">
+              <div>Nobody can equal him in intelligence</div>
+          </div>
+        </div>
+      </transition>
+
+      <div class="button_space">
+        <el-button type="primary" style="float: left">认识</el-button>
+        <el-button type="primary">模糊</el-button>
+        <el-button type="primary" style="float: right">忘记</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "Review",
+  data(){
+    return{
+      show: true
+    }
+  },
   methods: {
+    hide(){
+      this.show = !this.show;
+    },
     nextWord(){
       console.log("pre");
     },
@@ -27,19 +74,30 @@ export default {
 </script>
 
 <style scoped>
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 18px;
-  opacity: 0.75;
-  line-height: 300px;
-  margin: 0;
+.wordContent {
+  margin: auto 15%;
+  font-size: 13px;
+  text-align: left;
+  height: 500px;
+}
+.button_space {
+  margin: auto 15%;
+}
+.button_space button {
+  margin-top: 20px;
+  width: 20%;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
 }
 
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n+1) {
-  background-color: #d3dce6;
+.box-card {
+  width: 70%;
+  margin: auto;
 }
 </style>
